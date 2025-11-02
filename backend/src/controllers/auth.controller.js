33868852a897a7f4165da3fb9e08711e5f39abcd
RiddhiSharmaa <Riddhisharma9900@gmail.com>
2025-11-02 19:53:31 +0530

    Backend started

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const signup = async (req,res) => {
+    const {email, password, fullName} = req.body;
+}
+
+export const login = async (req, res) => {
+    res.send("Login Route");
+}
+
+export const logout = async (req, res) => {
+    res.send("Logout Route");
+}
